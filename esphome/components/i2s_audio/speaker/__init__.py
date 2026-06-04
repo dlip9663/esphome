f86d9c8e7195d6c2a52c594e7aedc773d2f4daa4
dlip9663 <59123436+dlip9663@users.noreply.github.com>
2026-06-04 19:40:41 -0400

    fix: remove ring_buffer from AUTO_LOAD (back to core API)

--- esphome/components/i2s_audio/speaker/__init__.py
+++ esphome/components/i2s_audio/speaker/__init__.py
@@ -31,7 +31,7 @@ from .. import (
     validate_mclk_divisible_by_3,
 )
 
-AUTO_LOAD = ["audio", "ring_buffer"]
+AUTO_LOAD = ["audio"]
 CODEOWNERS = ["@jesserockz", "@kahrendt"]
 DEPENDENCIES = ["i2s_audio"]
 
