0fea331979f5e2ff3165fc0039873026777b204d
dlip9663 <59123436+dlip9663@users.noreply.github.com>
2026-06-04 20:35:29 -0400

    add: esphome/components/sendspin/media_player/__init__.py from kahrendt/esphome

--- esphome/components/sendspin/media_player/__init__.py
+++ esphome/components/sendspin/media_player/__init__.py
@@ -1,11 +1,12 @@
+"""Sendspin Media Player Setup."""
+
 import esphome.codegen as cg
 from esphome.components import media_player
 from esphome.components.const import CONF_VOLUME_INCREMENT
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.types import ConfigType
 
-from .. import CONF_SENDSPIN_ID, SendspinHub, request_controller_support, sendspin_ns
+from .. import CONF_SENDSPIN_ID, SendspinHub, sendspin_ns
 
 CODEOWNERS = ["@kahrendt"]
 DEPENDENCIES = ["sendspin"]
@@ -16,30 +17,23 @@ SendspinMediaPlayer = sendspin_ns.class_(
     cg.Component,
 )
 
-
-def _request_roles(config: ConfigType) -> ConfigType:
-    """Request the necessary Sendspin roles for the media player."""
-    request_controller_support()
-
-    return config
-
-
-CONFIG_SCHEMA = cv.All(
-    media_player.media_player_schema(SendspinMediaPlayer).extend(
-        {
-            cv.GenerateID(CONF_SENDSPIN_ID): cv.use_id(SendspinHub),
-            cv.Optional(CONF_VOLUME_INCREMENT, default=0.05): cv.percentage,
-        }
-    ),
-    cv.only_on_esp32,
-    _request_roles,
+CONFIG_SCHEMA = media_player.media_player_schema(SendspinMediaPlayer).extend(
+    {
+        cv.GenerateID(): cv.declare_id(SendspinMediaPlayer),
+        cv.GenerateID(CONF_SENDSPIN_ID): cv.use_id(SendspinHub),
+        cv.Optional(CONF_VOLUME_INCREMENT, default=5): cv.int_range(min=1, max=50),
+    }
 )
 
 
-async def to_code(config: ConfigType) -> None:
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await cg.register_parented(var, config[CONF_SENDSPIN_ID])
     await media_player.register_media_player(var, config)
 
     cg.add(var.set_volume_increment(config[CONF_VOLUME_INCREMENT]))
+
+    cg.add_define("USE_SENDSPIN_CONTROLLER")
+
+    return var
