51539643efd45b1794102203097948e12655f51d
dlip9663 <59123436+dlip9663@users.noreply.github.com>
2026-06-04 20:35:32 -0400

    add: esphome/components/sendspin/media_source/__init__.py from kahrendt/esphome

--- esphome/components/sendspin/media_source/__init__.py
+++ esphome/components/sendspin/media_source/__init__.py
@@ -1,35 +1,20 @@
+"""Sendspin media source component for ESPHome."""
+
 from esphome import automation
 import esphome.codegen as cg
-from esphome.components import media_source
+from esphome.components import media_source, psram
 import esphome.config_validation as cv
-from esphome.const import (
-    CONF_BUFFER_SIZE,
-    CONF_ID,
-    CONF_SAMPLE_RATE,
-    CONF_TASK_STACK_IN_PSRAM,
-)
-from esphome.core import ID
-from esphome.cpp_generator import MockObj, TemplateArgsType
-from esphome.types import ConfigType
+from esphome.const import CONF_BUFFER_SIZE, CONF_ID, CONF_TASK_STACK_IN_PSRAM
 
-from .. import (
-    CONF_DECODE_MEMORY,
-    CONF_FIXED_DELAY,
-    CONF_INITIAL_STATIC_DELAY,
-    CONF_SENDSPIN_ID,
-    MEMORY_LOCATIONS,
-    SendspinHub,
-    _validate_task_stack_in_psram,
-    register_player_config,
-    request_controller_support,
-    sendspin_ns,
-)
+from .. import CONF_SENDSPIN_ID, SendspinHub, sendspin_ns
 
 AUTO_LOAD = ["audio"]
 CODEOWNERS = ["@kahrendt"]
+DEPENDENCIES = ["media_source", "audio"]
 
+CONF_INITIAL_STATIC_DELAY = "initial_static_delay"
 CONF_STATIC_DELAY_ADJUSTABLE = "static_delay_adjustable"
-
+CONF_FIXED_DELAY = "fixed_delay"
 
 SendspinMediaSource = sendspin_ns.class_(
     "SendspinMediaSource",
@@ -40,38 +25,25 @@ SendspinMediaSource = sendspin_ns.class_(
 EnableStaticDelayAdjustmentAction = sendspin_ns.class_(
     "EnableStaticDelayAdjustmentAction",
     automation.Action,
-    cg.Parented.template(SendspinMediaSource),
+    cg.Parented.template(SendspinHub),
 )
 
 DisableStaticDelayAdjustmentAction = sendspin_ns.class_(
     "DisableStaticDelayAdjustmentAction",
     automation.Action,
-    cg.Parented.template(SendspinMediaSource),
+    cg.Parented.template(SendspinHub),
 )
 
-
-def _register(config: ConfigType) -> ConfigType:
-    request_controller_support()
-    register_player_config(
-        {
-            CONF_SAMPLE_RATE: config[CONF_SAMPLE_RATE],
-            CONF_BUFFER_SIZE: config[CONF_BUFFER_SIZE],
-            CONF_INITIAL_STATIC_DELAY: config[CONF_INITIAL_STATIC_DELAY],
-            CONF_FIXED_DELAY: config[CONF_FIXED_DELAY],
-            CONF_TASK_STACK_IN_PSRAM: config.get(CONF_TASK_STACK_IN_PSRAM, False),
-            CONF_DECODE_MEMORY: config.get(CONF_DECODE_MEMORY),
-        }
-    )
-    return config
-
-
-CONFIG_SCHEMA = cv.All(
+CONFIG_SCHEMA = (
     media_source.media_source_schema(
         SendspinMediaSource,
-    ).extend(
+    )
+    .extend(
         {
             cv.GenerateID(CONF_SENDSPIN_ID): cv.use_id(SendspinHub),
-            cv.Optional(CONF_TASK_STACK_IN_PSRAM): _validate_task_stack_in_psram,
+            cv.Optional(CONF_TASK_STACK_IN_PSRAM): cv.All(
+                cv.boolean, cv.requires_component(psram.DOMAIN)
+            ),
             cv.Optional(CONF_BUFFER_SIZE, default=1000000): cv.int_range(min=25000),
             cv.Optional(CONF_INITIAL_STATIC_DELAY, default="0ms"): cv.All(
                 cv.positive_time_period_milliseconds,
@@ -82,18 +54,14 @@ CONFIG_SCHEMA = cv.All(
                 cv.positive_time_period_microseconds,
                 cv.Range(max=cv.TimePeriod(microseconds=10000)),
             ),
-            cv.Optional(CONF_SAMPLE_RATE, default=48000): cv.int_range(
-                min=16000, max=96000
-            ),
-            cv.Optional(CONF_DECODE_MEMORY): cv.one_of(*MEMORY_LOCATIONS, lower=True),
         }
-    ),
-    cv.only_on_esp32,
-    _register,
+    )
+    .extend(cv.COMPONENT_SCHEMA)
 )
 
 
-async def to_code(config: ConfigType) -> None:
+async def to_code(config):
+    """Generate code for sendspin media source."""
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await media_source.register_media_source(var, config)
@@ -101,38 +69,48 @@ async def to_code(config: ConfigType) -> None:
     sendspin_hub = await cg.get_variable(config[CONF_SENDSPIN_ID])
     await cg.register_parented(var, sendspin_hub)
 
-    cg.add(sendspin_hub.set_listener(var))
+    cg.add(sendspin_hub.set_buffer_size(config[CONF_BUFFER_SIZE]))
 
-    cg.add(var.set_static_delay_adjustable(config[CONF_STATIC_DELAY_ADJUSTABLE]))
+    if config[CONF_INITIAL_STATIC_DELAY].total_milliseconds > 0:
+        cg.add(
+            sendspin_hub.set_initial_static_delay_ms(config[CONF_INITIAL_STATIC_DELAY])
+        )
+
+    if config[CONF_STATIC_DELAY_ADJUSTABLE]:
+        cg.add(sendspin_hub.set_static_delay_adjustable(True))
+
+    if config[CONF_FIXED_DELAY].total_microseconds > 0:
+        cg.add(sendspin_hub.set_fixed_delay_us(config[CONF_FIXED_DELAY]))
+
+    cg.add_define("USE_SENDSPIN_PLAYER")
+    cg.add_define("USE_SENDSPIN_CONTROLLER")
+
+    if CONF_TASK_STACK_IN_PSRAM in config:
+        cg.add(var.set_task_stack_in_psram(config[CONF_TASK_STACK_IN_PSRAM]))
 
 
-SENDSPIN_MEDIA_SOURCE_ACTION_SCHEMA = automation.maybe_simple_id(
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.use_id(SendspinMediaSource),
-        }
-    )
+SENDSPIN_SIMPLE_ACTION_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.use_id(SendspinHub),
+    }
 )
 
 
 @automation.register_action(
     "sendspin.media_source.enable_static_delay_adjustment",
     EnableStaticDelayAdjustmentAction,
-    SENDSPIN_MEDIA_SOURCE_ACTION_SCHEMA,
+    SENDSPIN_SIMPLE_ACTION_SCHEMA,
     synchronous=True,
 )
 @automation.register_action(
     "sendspin.media_source.disable_static_delay_adjustment",
     DisableStaticDelayAdjustmentAction,
-    SENDSPIN_MEDIA_SOURCE_ACTION_SCHEMA,
+    SENDSPIN_SIMPLE_ACTION_SCHEMA,
     synchronous=True,
 )
 async def sendspin_static_delay_adjustment_to_code(
-    config: ConfigType,
-    action_id: ID,
-    template_arg: cg.TemplateArguments,
-    args: TemplateArgsType,
-) -> MockObj:
+    config, action_id, template_arg, args
+):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     return var
