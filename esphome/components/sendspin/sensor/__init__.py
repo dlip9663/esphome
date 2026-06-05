d634d333f4abdc7f56834e6a485bea4b290bdb9b
dlip9663 <59123436+dlip9663@users.noreply.github.com>
2026-06-04 20:35:59 -0400

    add: esphome/components/sendspin/sensor/__init__.py from kahrendt/esphome

--- esphome/components/sendspin/sensor/__init__.py
+++ esphome/components/sendspin/sensor/__init__.py
@@ -1,98 +1,80 @@
+"""Sendspin Sensor Setup."""
+
 import esphome.codegen as cg
 from esphome.components import sensor
 import esphome.config_validation as cv
 from esphome.const import (
     CONF_ID,
     CONF_TYPE,
-    CONF_YEAR,
+    ENTITY_CATEGORY_DIAGNOSTIC,
     STATE_CLASS_MEASUREMENT,
     UNIT_MILLISECOND,
 )
-from esphome.types import ConfigType
 
-from .. import CONF_SENDSPIN_ID, SendspinHub, request_metadata_support, sendspin_ns
+from .. import CONF_SENDSPIN_ID, SendspinHub, sendspin_ns
 
 CODEOWNERS = ["@kahrendt"]
 DEPENDENCIES = ["sendspin"]
 
-CONF_TRACK = "track"
-CONF_TRACK_PROGRESS = "track_progress"
-CONF_TRACK_DURATION = "track_duration"
-
-SendspinTrackProgressSensor = sendspin_ns.class_(
-    "SendspinTrackProgressSensor",
-    sensor.Sensor,
-    cg.PollingComponent,
-)
-SendspinMetadataSensor = sendspin_ns.class_(
-    "SendspinMetadataSensor",
+SendspinSensor = sendspin_ns.class_(
+    "SendspinSensor",
     sensor.Sensor,
     cg.Component,
 )
 
-SendspinNumericMetadataTypes = sendspin_ns.enum(
-    "SendspinNumericMetadataTypes", is_class=True
-)
-_METADATA_TYPE_ENUM = {
-    CONF_TRACK_DURATION: SendspinNumericMetadataTypes.TRACK_DURATION,
-    CONF_YEAR: SendspinNumericMetadataTypes.YEAR,
-    CONF_TRACK: SendspinNumericMetadataTypes.TRACK,
+SendspinSensorTypes = sendspin_ns.enum("SendspinSensorTypes", is_class=True)
+SENDSPIN_DIAGNOSTIC_SENSOR_TYPES = {
+    "kalman_error": SendspinSensorTypes.KALMAN_ERROR,
+}
+
+SENDSPIN_METADATA_SENSOR_TYPES = {
+    "track_progress": SendspinSensorTypes.TRACK_PROGRESS,
+    "track_duration": SendspinSensorTypes.TRACK_DURATION,
+}
+
+SENDSPIN_SENSOR_TYPES = {
+    **SENDSPIN_DIAGNOSTIC_SENSOR_TYPES,
+    **SENDSPIN_METADATA_SENSOR_TYPES,
 }
 
 
-def _request_roles(config: ConfigType) -> ConfigType:
-    """Request the necessary Sendspin roles for the sensor."""
-    request_metadata_support()
-
-    return config
-
-
-_HUB_ID_SCHEMA = cv.Schema({cv.GenerateID(CONF_SENDSPIN_ID): cv.use_id(SendspinHub)})
-
-
-def _metadata_schema(**sensor_kwargs):
-    """Schema for event-driven numeric metadata sensors (duration/year/track)."""
-    return (
-        sensor.sensor_schema(
-            SendspinMetadataSensor,
+def _sensor_schema(value):
+    """Create the appropriate sensor schema based on the sensor type."""
+    sensor_type = value.get(CONF_TYPE)
+    if sensor_type in SENDSPIN_METADATA_SENSOR_TYPES:
+        schema = sensor.sensor_schema(
             accuracy_decimals=0,
-            **sensor_kwargs,
+            state_class=STATE_CLASS_MEASUREMENT,
+            unit_of_measurement=UNIT_MILLISECOND,
         )
-        .extend(_HUB_ID_SCHEMA)
-        .extend(cv.COMPONENT_SCHEMA)
-    )
-
-
-CONFIG_SCHEMA = cv.All(
-    cv.typed_schema(
+    else:
+        schema = sensor.sensor_schema(
+            accuracy_decimals=0,
+            state_class=STATE_CLASS_MEASUREMENT,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        )
+    schema = schema.extend(
         {
-            CONF_TRACK_PROGRESS: sensor.sensor_schema(
-                SendspinTrackProgressSensor,
-                accuracy_decimals=0,
-                state_class=STATE_CLASS_MEASUREMENT,
-                unit_of_measurement=UNIT_MILLISECOND,
-            )
-            .extend(_HUB_ID_SCHEMA)
-            .extend(cv.polling_component_schema("1s")),
-            CONF_TRACK_DURATION: _metadata_schema(
-                state_class=STATE_CLASS_MEASUREMENT,
-                unit_of_measurement=UNIT_MILLISECOND,
-            ),
-            CONF_YEAR: _metadata_schema(),
-            CONF_TRACK: _metadata_schema(),
-        },
-        key=CONF_TYPE,
-    ),
-    cv.only_on_esp32,
-    _request_roles,
-)
+            cv.GenerateID(): cv.declare_id(SendspinSensor),
+            cv.GenerateID(CONF_SENDSPIN_ID): cv.use_id(SendspinHub),
+            cv.Required(CONF_TYPE): cv.enum(SENDSPIN_SENSOR_TYPES),
+        }
+    )
+    return schema(value)
 
 
-async def to_code(config: ConfigType) -> None:
+CONFIG_SCHEMA = _sensor_schema
+
+
+async def to_code(config):
+    cg.add_define("USE_SENDSPIN_SENSOR", True)
+
+    if config[CONF_TYPE] in SENDSPIN_METADATA_SENSOR_TYPES.values():
+        cg.add_define("USE_SENDSPIN_METADATA", True)
+
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await cg.register_parented(var, config[CONF_SENDSPIN_ID])
     await sensor.register_sensor(var, config)
 
-    if (metadata_type := _METADATA_TYPE_ENUM.get(config[CONF_TYPE])) is not None:
-        cg.add(var.set_metadata_type(metadata_type))
+    cg.add(var.set_sensor_type(config[CONF_TYPE]))
