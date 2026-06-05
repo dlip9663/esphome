eaf879f45c29b6acfa13075d206af923ecb6c060
dlip9663 <59123436+dlip9663@users.noreply.github.com>
2026-06-04 20:35:31 -0400

    add: esphome/components/sendspin/media_player/sendspin_media_player.h from kahrendt/esphome

--- esphome/components/sendspin/media_player/sendspin_media_player.h
+++ esphome/components/sendspin/media_player/sendspin_media_player.h
@@ -7,27 +7,30 @@
 #include "esphome/components/media_player/media_player.h"
 #include "esphome/components/sendspin/sendspin_hub.h"
 
-namespace esphome::sendspin_ {
+#include "esphome/core/component.h"
 
-class SendspinMediaPlayer : public SendspinChild, public media_player::MediaPlayer {
+namespace esphome {
+namespace sendspin {
+
+class SendspinMediaPlayer : public Component, public media_player::MediaPlayer, public Parented<SendspinHub> {
  public:
+  float get_setup_priority() const override { return esphome::setup_priority::AFTER_CONNECTION; }
   void setup() override;
-  void dump_config() override;
+  void loop() override;
 
   // MediaPlayer implementations
   media_player::MediaPlayerTraits get_traits() override;
 
-  void set_volume_increment(float volume_increment) { this->volume_increment_ = volume_increment; }
-
-  bool is_muted() const override { return this->muted_; }
+  void set_volume_increment(uint8_t volume_increment) { this->volume_increment_ = volume_increment; }
 
  protected:
   // Receives commands from HA
   void control(const media_player::MediaPlayerCall &call) override;
 
-  float volume_increment_{0.05f};
-  bool muted_{false};
+  uint8_t volume_increment_{5};
+  bool force_publish_state_{false};
 };
 
-}  // namespace esphome::sendspin_
+}  // namespace sendspin
+}  // namespace esphome
 #endif
